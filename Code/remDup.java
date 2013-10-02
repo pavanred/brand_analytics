ae286eb6855ee0014ef274e384940521ecc38cea
asifmansoor <asifmansoor.a@gmail.com>
2013-10-02 10:11:50 -0500

    Code for Removing duplicates
    Added remDup.java code for removing duplicates in the keyvalue pair,
    and also the output from the reduce will be in the following form :
    userid brandid
    
    Sample Input and output data to check the code
    
    Made some changes in Readme file

---
+++ Code/remDup.java
@@ -0,0 +1,75 @@
+package org.apache.hadoop.examples;
+
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.*;
+
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.*;
+
+import org.apache.hadoop.mapreduce.*;
+
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+//import org.apache.hadoop.mapred.KeyValueTextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.input.*;
+import org.apache.hadoop.mapreduce.lib.output.*;
+
+import org.apache.hadoop.util.*;
+import org.apache.hadoop.util.GenericOptionsParser;
+
+public class remDup extends Configured implements Tool {
+public static class MapClass
+extends Mapper<LongWritable, Text, Text, IntWritable> {
+	private final static IntWritable one = new IntWritable(1);
+public void map(LongWritable key, Text value, Context context)
+throws IOException, InterruptedException {
+	// join key and value 
+	//System.out.print(value);
+String[] str =value.toString().split(" ");
+String cmbnd = str[0]+","+str[1];
+context.write(new Text(cmbnd), one);
+}
+}
+public static class Reduce extends Reducer<Text, IntWritable, Text, Text> {
+public void reduce(Text key, Iterable<IntWritable> values,
+Context context)
+throws IOException, InterruptedException {
+String[] keyvalues = key.toString().split(",");
+
+context.write(new Text(keyvalues[1]), new Text(keyvalues[0]));
+}
+}
+public int run(String[] args) throws Exception {
+Configuration conf = getConf();
+Job job = new Job(conf, "MyJob");
+job.setJarByClass(remDup.class);
+Path in = new Path(args[0]);
+Path out = new Path(args[1]);
+FileInputFormat.setInputPaths(job, in);
+FileOutputFormat.setOutputPath(job, out);
+job.setMapperClass(MapClass.class);
+job.setReducerClass(Reduce.class);
+// Setting to KeyValue Text Input format class, so that the key and value is text
+job.setInputFormatClass(TextInputFormat.class); 
+job.setOutputFormatClass(TextOutputFormat.class);
+job.setMapOutputKeyClass(Text.class);
+job.setMapOutputValueClass(IntWritable.class);
+
+job.setOutputKeyClass(Text.class);
+job.setOutputValueClass(Text.class);
+System.exit(job.waitForCompletion(true)?0:1);
+return 0;
+}
+public static void main(String[] args) throws Exception {
+int res = ToolRunner.run(new Configuration(), new remDup(), args);
+System.exit(res);
+}
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 brand_analytics
 ===============
+
+The objective of the project is to use large scale of data about brands and their respective user activity from social media networks, analyze the data to group brands based on user activities and use community detection algorithms for identifying similar brands, predict brand recommendations, brand trends, brand advertising etc. We plan to use data involving brands and user activity on brand pages from Facebook for this project.
